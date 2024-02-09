240479d966012c1fe50f78d14de98a8075ff77c9
Lara <jlara@usaid.gov>
2024-02-09 16:45:58 +0200

    add function parse_sisma_hiv_dah

---
+++ R/parse_sisma_hiv_dah.R
@@ -0,0 +1,41 @@
+#' A specific helper function for parsing a cleaned CSV export from SISMA
+#'
+#' @param file Dataframe cleaned via reshape_sisma
+#'
+#' @return A tidy format of SISMA dataframe
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'
+#'  df <- parse_sisma_hiv_dah()}
+
+parse_sisma_hiv_dah <- function(file) {
+
+  df <- file %>%
+
+    dplyr::left_join(data_sisma_hiv_dah_map, by = "indicator") %>%
+    dplyr::filter(!is.na(value)) %>%
+    dplyr::mutate(period_cohort = NA,
+                  disaggregate_sub = NA_character_,
+                  result_status = NA_character_) %>%
+    dplyr::select(sisma_uid,
+                  snu,
+                  psnu,
+                  sitename,
+                  period,
+                  period_cohort,
+                  indicator = indicator_new,
+                  source,
+                  disaggregate,
+                  disaggregate_sub,
+                  sub_group,
+                  sex,
+                  age,
+                  age_coarse,
+                  result_status,
+                  value)
+
+  return(df)
+
+}
