232e3bb88907cd16b17129593f1134b6bab71ce4
Lara <jlara@usaid.gov>
2024-02-15 23:18:07 +0200

    add sisma search words in parse_sisma_csv

--- R/parse_sisma_csv.R
+++ R/parse_sisma_csv.R
@@ -3,7 +3,7 @@
 #'
 #' @description parse_sisma_csv cleans and transforms programmatic
 #' data sets exported from SISMA into tidy data frames. Such
-#' structured data sets are key to facilitating subsequent analysis
+#' structured data sets are key to facilitating indicator analysis
 #' in R/PowerBI/Tableau.
 #'
 #' ## Overview of programmatic data type options
@@ -26,7 +26,7 @@
 #'
 #' @param data Path to SISMA dataset (saved in .csv format)
 #' @param type Defines programmatic data type for processing
-#' @return A tidy dataframe with monthly sisma results
+#' @return A tidy SISMA programmatic dataframe
 #' @export
 #'
 #' @examples
@@ -37,22 +37,22 @@
 parse_sisma_csv <- function(data, type){
 
   switch(type,
-         "SMI-CPN" = parse_sisma_smi_cpn(data),
-         "SMI-MAT" = parse_sisma_smi_mat(data),
-         "SMI-CCR" = parse_sisma_smi_ccr(data),
-         "ATS Result" = parse_sisma_ats_results(data), # search term "ano"
-         "ATS History" = parse_sisma_ats_history(data),
-         "ATS CI" = parse_sisma_ats_index(data),
-         "ATS SAAJ" = parse_sisma_ats_saaj_cm(data),
+         "SMI-CPN" = parse_sisma_smi_cpn(data), # SISMA search term "SMI-CPN"
+         "SMI-MAT" = parse_sisma_smi_mat(data), # SISMA search term "SMI-MAT"
+         "SMI-CCR" = parse_sisma_smi_ccr(data), # SISMA search term "SMI - CCR - "
+         "ATS Result" = parse_sisma_ats_results(data), # SISMA search term "ano" within Grupo de Elemento de Dados "ATS - Resumo Mensal (Nova)"
+         "ATS History" = parse_sisma_ats_history(data), # SISMA search terms "Historial" & "chave" within Grupo de Elemento de Dados "ATS - Resumo Mensal (Nova)"
+         "ATS CI" = parse_sisma_ats_index(data),  # SISMA search terms "diagonsticados" & "indice" within Grupo de Elemento de Dados "ATS - Resumo Mensal (Nova)"
+         "ATS SAAJ" = parse_sisma_ats_saaj_cm(data), # SISMA search terms "MZ SAAJ - Testagem para HIV -" & "MZ C.MASC - Testados"
          "ATS CCSD" = parse_sisma_ats_ccsd(data),
          "ATS SMI" = parse_sisma_ats_smi(data),
          "ATS Auto" = parse_sisma_ats_auto(data),
-         "HIV TARV" = parse_sisma_hiv_tarv(data),
-         "HIV PREP" = parse_sisma_hiv_prep(data),
-         "HIV APSS" = parse_sisma_hiv_apss(data),
-         "HIV ITS" = parse_sisma_hiv_its(data),
-         "HIV AJMHCMM" = parse_sisma_hiv_ajm_hc_mm(data),
-         "HIV DAH" = parse_sisma_hiv_dah(data),
+         "HIV TARV" = parse_sisma_hiv_tarv(data), # SISMA search term "MZ HIV SIDA - "
+         "HIV PREP" = parse_sisma_hiv_prep(data), # SISMA search term "MZ PREP"
+         "HIV APSS" = parse_sisma_hiv_apss(data), # SISMA search term "MZ APSS PP"
+         "HIV ITS" = parse_sisma_hiv_its(data), # SISMA search term "MZ ITS -"
+         "HIV AJMHCMM" = parse_sisma_hiv_ajm_hc_mm(data), # SISMA search term "MZ AJMHCMM"
+         "HIV DAH" = parse_sisma_hiv_dah(data), # SISMA search term "MZ DAH -"
   )
 
 }
