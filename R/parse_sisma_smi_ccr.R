3524e2ad9ab84c47a9cb39024206d45e340dd3ed
Lara <jlara@usaid.gov>
2024-05-09 13:04:58 +0200

    modification to parse_sisma_smi_ccr to align with new CCR var mapping

--- R/parse_sisma_smi_ccr.R
+++ R/parse_sisma_smi_ccr.R
@@ -26,8 +26,7 @@ parse_sisma_smi_ccr <- function(df) {
                   age = NA_character_,
                   sub_group = NA_character_,
                   age_coarse = NA_character_,
-                  result_status = NA_character_,
-                  disaggregate_sub = NA_character_) %>%
+                  result_status = NA_character_) %>%
     dplyr::select(sisma_uid,
                   snu,
                   psnu,
