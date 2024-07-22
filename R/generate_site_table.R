d6e477bc416afd60900d51cd45162c750d63b9a4
Lara <jlara@usaid.gov>
2024-07-22 13:11:59 +0200

    update generate_site_table

--- R/generate_site_table.R
+++ R/generate_site_table.R
@@ -15,8 +15,10 @@
 generate_site_table <- function(file) {
 
   df <- readr::read_csv(file) |>
-    dplyr::distinct(orgunitlevel2, orgunitlevel3, orgunitlevel4, organisationunitid, .keep_all = TRUE) |>
+    dplyr::distinct(orgunitlevel2, orgunitlevel3, orgunitlevel4, organisationunitid) |>
     dplyr::mutate(dplyr::across(c(orgunitlevel2, orgunitlevel3), ~ stringr::str_to_title(.))) |>
+    dplyr::mutate(orgunitlevel4 = dplyr::case_when(organisationunitid == "abg5UReivZX" ~ "Privado Dream Comunidade Santo Egidio Zimpeto",
+                                                   .default = orgunitlevel4)) |>
     dplyr::select(sisma_uid = organisationunitid,
                   provincia = orgunitlevel2,
                   distrito = orgunitlevel3,
