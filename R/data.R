8bdf220afbcfa5a48fc7cd05786cbb570ee3e027
Lara <jlara@usaid.gov>
2024-07-22 13:24:51 +0200

    addition of data_disa_uid_map

--- R/data.R
+++ R/data.R
@@ -421,3 +421,17 @@
 #' }
 "data_sisma_sitelist"
 
+
+#' UID map for linking DISA, SISMA and DATIM
+#'
+#' List of indicators
+#'
+#' @usage data(data_disa_uid_map)
+#'
+#' @format A list of the variables included in the site list
+#' \describe{
+#'   \item{data_disa_uid_map}{disa_uid}
+#'   \item{data_disa_uid_map}{sisma_uid}
+#'   \item{data_disa_uid_map}{datim_uid}
+#' }
+"data_disa_uid_map"
