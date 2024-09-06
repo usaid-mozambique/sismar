c440d1f4e08d53b923332423361b0515428d1bf7
Lara <jlara@usaid.gov>
2024-09-06 11:15:42 +0200

    revise generation and saving of internal template data

--- R/process_sisma_export.R
+++ R/process_sisma_export.R
@@ -18,33 +18,6 @@ process_sisma_export <- function(filename, language = "portuguese") {
   df <- readr::read_csv(filename) |>
     janitor::clean_names()
 
-  # load submission templates into named list
-  # templates <- list(
-  #   parse_sisma_ats_results = template_ats_results |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_ats_history = template_ats_hist |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_ats_index = template_ats_ci |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_ats_ccsd = template_ats_ccsd |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_ats_saaj_cm = template_ats_saaj |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_ats_smi = template_ats_smi |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_ats_auto = template_ats_auto |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_hiv_dah = template_hiv_dah |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_hiv_tarv = template_hiv_tarv |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_hiv_prep = template_hiv_prep |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_hiv_apss = template_hiv_apss |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_hiv_its = template_hiv_its |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_hiv_ajm_hc_mm = template_hiv_ajmhcmm |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_cpn = template_smi_cpn |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_mat = template_smi_mat |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_ccr = template_smi_ccr |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_cpp = template_smi_cpp |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_pav = template_smi_pav |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_ccd = template_smi_ccd |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_ccs = template_smi_ccs |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_ug = template_smi_ug |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_pf = template_smi_pf |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols),
-  #   parse_sisma_smi_pf_int = template_smi_pf_int |> dplyr::distinct(input_cols) |> dplyr::pull(input_cols)
-  # )
-
   templates <- input_templates
 
   # compare input file with templates and return match results
