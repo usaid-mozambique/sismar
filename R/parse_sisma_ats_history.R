20cba3cd9a8d7c40a0e7f0500daf7b43b6724242
Joseph Lara <joseph.lara@gmail.com>
2025-02-11 10:33:06 +0200

    **feat** simplify parse_sisma_ats_history with add_missing_vars and seq_vars

--- R/parse_sisma_ats_history.R
+++ R/parse_sisma_ats_history.R
@@ -20,39 +20,19 @@
 
 parse_sisma_ats_history <- function(df) {
 
-  df_all <- df %>%
+  df <- df %>%
     dplyr::left_join(data_sisma_ats_hist_map, by = "indicator") %>%
     dplyr::filter(!is.na(value)) %>%
-    dplyr::mutate(
-      period_cohort = as.Date(NA),
-      disaggregate_sub = NA_character_,
-      age_coarse = NA_character_,
-      source = "LdR ATS",
-      age = NA_character_,
-      sex = NA_character_)
+    dplyr::mutate(source = "LdR ATS")
 
-  df_pos <- df_all %>%
+  df_pos <- df %>%
     dplyr::filter(result_status == "Positivo") %>%
-    dplyr::mutate(indicator = dplyr::case_when(indicator == "ATS_KP" ~ "ATS_KP_POS"))
+    dplyr::mutate(indicator = "ATS_KP_POS")
 
-  df_parse <- dplyr::bind_rows(df_all, df_pos) %>%
-    dplyr::select(sisma_uid,
-                  snu,
-                  psnu,
-                  sitename,
-                  period,
-                  period_cohort,
-                  indicator = indicator_new,
-                  source,
-                  disaggregate,
-                  disaggregate_sub,
-                  sub_group,
-                  sex,
-                  age_coarse,
-                  age,
-                  result_status,
-                  value)
+  df <- dplyr::bind_rows(df, df_pos) %>%
+    add_missing_vars() %>%
+    seq_vars()
 
-  return(df_parse)
+  return(df)
 
 }
