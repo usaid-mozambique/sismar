ca9a8991705c8ec711743185cf1eafc077e5a3d3
Joseph Lara <joseph.lara@gmail.com>
2025-02-11 10:49:16 +0200

    **feat** simplify parse_sisma_hiv_prep with add_missing_vars and seq_vars

--- R/parse_sisma_hiv_prep.R
+++ R/parse_sisma_hiv_prep.R
@@ -24,31 +24,14 @@ parse_sisma_hiv_prep <- function(df) {
 
     dplyr::left_join(data_sisma_hiv_prep_map, by = "indicator") %>%
     dplyr::filter(!is.na(value)) %>%
-    dplyr::mutate(period_cohort = as.Date(NA),
-                  source = "LdR PrEP",
-                  disaggregate_sub = NA_character_,
-                  sub_group = NA_character_,
+    dplyr::mutate(source = "LdR PrEP",
                   age_coarse = dplyr::case_when(age == "15-19" ~ "15+", # correct
                                                 age == "20-24" ~ "15+",
                                                 age == "25-29" ~ "15+",
-                                                age == "25+" ~ "15+"),
-                  result_status = NA_character_) %>%
-    dplyr::select(sisma_uid,
-                  snu,
-                  psnu,
-                  sitename,
-                  period,
-                  period_cohort,
-                  indicator = indicator_new,
-                  source,
-                  disaggregate,
-                  disaggregate_sub,
-                  sub_group,
-                  sex,
-                  age,
-                  age_coarse,
-                  result_status,
-                  value)
+                                                age == "25+" ~ "15+")
+                  ) %>%
+    add_missing_vars() %>%
+    seq_vars()
 
   return(df)
 
