39c81adc179297052be81f06bdb6633cdf3604e8
Joseph Lara <joseph.lara@gmail.com>
2025-02-14 08:27:35 +0200

    **fix** consolidation of package utils and fix of package dependencies in documention

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,50 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
+
+
+#' Cria uma estrutura organizacional (pastas) que é comum a todos os projectos sismar
+#'
+#' @param folder_list lista de pastas a instalar
+#'
+#' @return Um conjunto padrão de pastas no projeto para utilizar com as funções `sismar`
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'
+#'  setup_sismar_folder()}
+
+setup_sismar_folder <- function(
+
+  folder_list = list("Data", "Data/sisma", "Data/disa", "Data/pepfar", "Data/ine", # data
+                     "Data/sismar/processed", "Data/disa/processed", "Data/pepfar/processed", # data processed
+                     "Images", "Scripts", "AI", "Dataout", "Data_public", "GIS", "Documents", "Graphics", "markdown") # other
+)
+
+{
+
+  if(!is.list(folder_list))
+    stop("Please provide a list of directories to create for the project.")
+  usethis::ui_info("The following directories will be created (if they do no already):")
+  print(glue::glue(crayon::green('{folder_list}')))
+  suppressWarnings(
+    purrr::walk(folder_list, ~dir.create(.))
+  )
+
+}
+
 
 utils::globalVariables(c("organisationunitcode","organisationunitdescription","organisationunitid",
                          "organisationunitname", "orgunitlevel1", "orgunitlevel2", "orgunitlevel3",
